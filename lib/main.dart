caf4b3b2e4d5b573f856713ba762721beac2aafa
GCosta <gcosta@rede.ulbra.br>
2024-06-21 04:28:11 -0300

    Primeira versão da calculadora de IMC

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calculadora de IMC',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(107, 107, 106, 1)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Calculadora de IMC'),
     );
   }
 }
@@ -55,71 +40,151 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int? _iconeSelecionado;
+  double _peso = 0;
+  double _altura = 0;
+  bool _pesoInvalido = true;
+  bool _alturaInvalida = true;
+  double _imc = 0;
+  String _imcCategoria = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Widget iconeSelecionavel(int index, {required String text, required IconData icon}) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: InkResponse(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon,
+              size: 48,
+              color: _iconeSelecionado == index ? Colors.blue : null,
+            ),
+            Text(
+              text,
+              style: TextStyle(
+                color: _iconeSelecionado == index ? Colors.blue : null
+              )
+            ),
+          ],
+        ),
+        onTap: () => setState(
+          () {
+            _iconeSelecionado = index;
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Calculadora de IMC',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  iconeSelecionavel(0, text: "Masculino", icon: Icons.male),
+                  iconeSelecionavel(1, text: "Feminino", icon: Icons.female),
+                ],
+              ),
             ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Seu peso (KG)',
+                          style: TextStyle(color: _pesoInvalido ? Colors.red : null),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            textAlign: TextAlign.center,
+                            onChanged: (value) => setState(
+                              () {
+                                var peso = double.tryParse(value);
+                                _pesoInvalido = (peso is! double) || peso <= 0;
+                                if (!_pesoInvalido) {
+                                  _peso = peso!;
+                                  calcularImc();
+                                }
+                              }
+                            ),
+                          )
+                        )
+                      ],
+                      )
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Sua altura (m)',
+                          style: TextStyle(color: _alturaInvalida ? Colors.red : null),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            textAlign: TextAlign.center,
+                            onChanged: (value) => setState(
+                              () {
+                                var altura = double.tryParse(value);
+                                _alturaInvalida = (altura is! double) || altura <= 0;
+                                if (!_alturaInvalida) {
+                                  _altura = altura!;
+                                  calcularImc();
+                                }
+                              }
+                            ),
+                          )
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ),
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text('Seu IMC'),
+                  Text(_pesoInvalido || _alturaInvalida ? "INVÁLIDO" : _imc.toStringAsFixed(2), style: const TextStyle(fontWeight: FontWeight.bold)),
+                  Text(_pesoInvalido || _alturaInvalida ? "Preencha peso e altura corretamente nos campos acima" : _imcCategoria)
+                ],
+              )
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void calcularImc() {
+    _imc = _peso / (_altura * _altura);
+    _imcCategoria = switch (_imc) {
+      < 18.5 => 'Abaixo do peso',
+      >= 18.5 && < 24.9 => 'Peso normal',
+      >= 24.9 && < 29.9 => 'Sobrepeso',
+      >= 29.9 => 'Obesidade',
+      _ => 'ERRO'
+    };
   }
 }
